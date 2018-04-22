ef856644a73bf070a73292537d5a4ce02eb906ac
Michelle Edmondson <GeeksRock@users.noreply.github.com>
2018-04-22 14:42:21 -0500

    Create deleteAllVisitNotes.php

---
+++ php/deleteAllVisitNotes.php
@@ -0,0 +1,26 @@
+<?php
+        require 'utils.php';
+        $link = mysqli_connect($server, $user_name, $pass_word, $data_base);
+		
+        $logged_in_user_id = isset($_POST['logged_in_user_id']) ? getSecureInputData($_POST['logged_in_user_id']) : null;
+        $logged_in_user_email = isset($_POST['logged_in_user_email']) ? getSecureInputData($_POST['logged_in_user_email']) : null;
+  
+        $visit_id = isset($_POST['visit_id']) ? getSecureInputData($_POST['visit_id']) : null;
+
+        if ($link) {
+           $isLoggedIn = isLoggedIn($logged_in_user_email, $logged_in_user_id, $link);
+           if ($isLoggedIn) {
+               $sqlDelete = "DELETE FROM notes 
+							WHERE visit_id = '$visit_id'";
+    
+                if ($link->query($sqlDelete) === TRUE) {
+                      echo "Notes deleted successfully";
+                } else {
+                      echo "An error occurred while deleting visit notes: " . $sqlDelete . "<br>" . $link->error;
+                }
+           } else {
+                forbidden();
+           }
+        }
+        mysqli_close($link);
+?>
