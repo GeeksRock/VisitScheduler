4283c166f2a52e5cf3b67705f1cfc147bed22f80
Michelle Edmondson <GeeksRock@users.noreply.github.com>
2018-04-22 17:38:43 -0500

    Create getUsers.php

---
+++ php/getUsers.php
@@ -0,0 +1,28 @@
+<?php
+        require 'utils.php';
+        $link = mysqli_connect($server, $user_name, $pass_word, $data_base);
+		
+        $logged_in_user_id = isset($_POST['logged_in_user_id']) ? getSecureInputData($_POST['logged_in_user_id']) : null;
+        $logged_in_user_email = isset($_POST['logged_in_user_email']) ? getSecureInputData($_POST['logged_in_user_email']) : null;
+
+        $emparray = array();
+
+        if ($link) {
+          $isLoggedIn = isLoggedIn($logged_in_user_email, $logged_in_user_id, $link);
+          if ($isLoggedIn) {
+                $sql = "SELECT id as Id,isadmin as IsAdmin,firstname as FirstName,lastname as LastName,phone as Phone,email as Email,active as Active
+						FROM users WHERE default_admin = '0'";
+                $result = mysqli_query($link, $sql);
+
+                if ($result) {              
+                    while ($row = mysqli_fetch_object($result)) {
+                        $emparray[] = $row;
+                    }
+                }
+                echo json_encode($emparray);
+          } else {
+              forbidden();
+          }            
+            mysqli_close($link);
+        }
+?>
