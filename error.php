b8b1627fcb3097ac25e7116a110eb98048c931b8
Michelle Edmondson <GeeksRock@users.noreply.github.com>
2018-04-22 19:07:03 -0500

    Create error.php

---
+++ error.php
@@ -0,0 +1,52 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Member Care Ministries | 403 Forbidden</title>    
+        
+        <script src="libs/jquery-3.1.1.min.js"></script>
+       <script src="js/utils.js"></script>
+       <link rel="stylesheet" href="libs/bootstrap-4.0.0/css/bootstrap.css">
+       <script src="libs/bootstrap-4.0.0/js/bootstrap.js"></script>
+       <link rel="stylesheet" href="libs/font-awesome-4.7.0/css/font-awesome.css"> 
+        <link rel="stylesheet" href="css/app.css">
+        
+    </head>
+    <body>
+          <div class="container">
+              <table class="table">
+                  <tbody>
+                      <tr>
+                          <td class="text-center">
+                              <h1>Member Care Ministries | 403 Forbidden</h1>
+                              <hr/>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td class="text-center">
+                              <img src="img/mcm_cross.png"/>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td class="text-md-center">
+                              <h3>Hi there. We're not sure how you ended up here,</h3><br/><h3>but you do not have access to this area.</h3>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td class="text-left" style="padding-left:21%!important;padding-top:5%!important">
+                              <h6>God bless. Have a nice day.</h6>
+                          </td>                              
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+          <div class="pusher"></div>
+          <footer class="footer">
+                <div class="container">
+                  <span class="text-muted">Powered with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="#">clac.com</a> | <a href="#">codelikeachicken.com</a></span>
+                </div>
+          </footer>
+    </body>
+</html>
